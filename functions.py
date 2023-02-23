4ba968ae99f2ee67a990e8cead5148f00ed5209d
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2023-02-23 22:48:34 +0900

    modified default value

--- functions.py
+++ functions.py
@@ -20,7 +20,7 @@ class AdjustSpeechRMS:
         figure_dir,
         speech_rms_set=0.05,
         noise_snr_set=30,
-        noise_threshold_snr=15,
+        noise_threshold_snr=20,
         sr=48000,
         subtype="PCM_16",
     ) -> None:
