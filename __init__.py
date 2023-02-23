a89c208263164157ea4a30cc901011b49032bbc0
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2023-02-23 16:07:44 +0900

    added wav_convert

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from .functions import AdjustSpeechRMS
+from .utils import wav_convert
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import soundfile as sf
+from scipy import signal
 
 
 def wavread(fn):
@@ -9,3 +12,13 @@ def wavread(fn):
 
 def wavwrite(fn, data, sr, subtype):
     sf.write(fn, data, sr, subtype)
+
+
+def wav_convert(fn, sr_new=48000, new_subtype="PCM_16"):
+    # transform any wav file to monaural signal
+    assert Path(fn).suffix == ".wav"
+    data, sr_org = sf.read(fn)
+    if len(data) > 1:
+        data = data[:, 0]
+    data = signal.resample(data, int(len(data) * sr_new / sr_org))
+    sf.write(fn, data, sr_new, new_subtype)
