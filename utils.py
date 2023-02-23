68ad5c9c287f7b4784f629df494a1bbecd431d73
TakuyaFujimura <fujimura.takuya@g.sp.m.is.nagoya-u.ac.jp>
2023-02-23 18:48:03 +0900

    added wav_split

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import numpy as np
 import soundfile as sf
 
 
@@ -9,3 +12,27 @@ def wavread(fn):
 
 def wavwrite(fn, data, sr, subtype):
     sf.write(fn, data, sr, subtype)
+
+
+def wav_split(filepath, n_split):
+    filepath = Path(filepath)
+    data, sr, subtype = wavread(filepath)
+    data_len = data.shape[0]  # n_time
+    split_len = data_len // n_split
+    for i in range(n_split - 1):
+        start = i * split_len
+        end = (i + 1) * split_len
+        output_path = filepath.parent / f"{filepath.stem}_{i}.wav"
+        wavwrite(output_path, data[start:end], sr, subtype)
+    output_path = filepath.parent / f"{filepath.stem}_{i+1}.wav"
+    wavwrite(output_path, data[end:], sr, subtype)
+
+
+def wav_concat(filepath_list, output_path):
+    all_data = []
+    for filepath in filepath_list:
+        filepath = Path(filepath)
+        data, sr, subtype = wavread(filepath)
+        all_data.append(data)
+    concat_data = np.concatenate(all_data, axis=0)
+    wavwrite(output_path, concat_data, sr, subtype)
